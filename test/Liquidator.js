df3f660b3acce9189516fe1e1830136364e5defd
smrm-dev <mrmousavi78@gmail.com>
2022-09-19 12:55:36 +0430

    Write tests for `placeBid`

--- test/Liquidator.js
+++ test/Liquidator.js
@@ -4,9 +4,11 @@ const {
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
-const { deploy, takeOutLoan, liquidate, ZERO } = require("./Bank");
+const { deploy, takeOutLoan, liquidate, ZERO, ONE } = require("./Bank");
 
 const ONLY_BANK = "ONLY_BANK";
+const NOT_ACTIVE_LIQUIDATION = "NOT_ACTIVE_LIQUIDATION";
+const INADEQUATE_BIDDING = "INADEQUATE_BIDDING";
 
 const LiquidationState = {
     ACTIVE: 0,
@@ -38,6 +40,32 @@ describe("Liquidator", function () {
             expect(lastLiquidation.collateral).to.equal(loan.collateral);
             expect(lastLiquidation.amount).to.equal(loan.amont);
             expect(lastLiquidation.state).to.equal(LiquidationState.ACTIVE);
+        });
+    });
+
+    describe("Place Bid", function () {
+        it("Should fail because of not active liquidation", async function () {
+            const { liquidator } = await loadFixture(liquidate);
+            await expect(liquidator.placeBid(ZERO, ZERO)).to.be.rejectedWith(NOT_ACTIVE_LIQUIDATION);
+        });
+
+        it("Should fail because of inadequate bidding", async function () {
+            const { bank, liquidator, loanId } = await loadFixture(liquidate);
+            const lastLiquidationId = await liquidator.lastLiquidationId;
+            const loan = await bank.loans(loanId);
+
+            await expect(liquidator.placeBid(lastLiquidationId, loan.collateral.add(ONE))).to.be.rejectedWith(INADEQUATE_BIDDING);
+        });
+
+        it("Should place bid", async function () {
+            const { bank, liquidator, loanId, owner } = await loadFixture(liquidate);
+            const lastLiquidationId = await liquidator.lastLiquidationId;
+            const loan = await bank.loans(loanId);
+            await liquidator.placeBid(lastLiquidationId, loan.collateral);
+            const lastLiquidation = await liquidator.liquidations(lastLiquidationId);
+
+            expect(lastLiquidation.bestBidder).to.equal(owner.address);
+            expect(lastLiquidation.bestBid).to.equal(loan.collateral);
         });
     });
 });
