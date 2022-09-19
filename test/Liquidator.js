ce6bfcf7f67a619e0d141c08617babfa8aeec02a
smrm-dev <mrmousavi78@gmail.com>
2022-09-19 11:38:26 +0430

    Write tests for `startLiquidation`

--- test/Liquidator.js
+++ test/Liquidator.js
@@ -4,25 +4,40 @@ const {
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { deploy, takeOutLoan, liquidate, ZERO } = require("./Bank");
+
+const ONLY_BANK = "ONLY_BANK";
+
+const LiquidationState = {
+    ACTIVE: 0,
+    FINISHED: 1
+}
 
 describe("Liquidator", function () {
     // We define a fixture to reuse the same setup in every test.
     // We use loadFixture to run this setup once, snapshot that state,
     // and reset Hardhat Network to that snapshot in every test.
-    async function deploy() {
-
-        // Contracts are deployed using the first signer/account by default
-        const [owner, otherAccount] = await ethers.getSigners();
-
-        const Liquidator = await ethers.getContractFactory("Liquidator");
-        const liquidator = await Liquidator.deploy();
-
-        return { liquidator, owner, otherAccount };
-    }
-
     describe("Deployment", function () {
         it("Always Fail", async function () {
             expect(true).to.equal(false);
+        });
+    });
+
+    describe("Start Liquidation", function () {
+        it("Should fail because of being called by an address which is not Bank", async function () {
+            const { liquidator } = await loadFixture(deploy);
+            await expect(liquidator.startLiquidation(ZERO, ZERO, ZERO, ZERO)).to.be.rejectedWith(ONLY_BANK);
+        });
+
+        it("Should start liquidation", async function () {
+            const { bank, liquidator, loanId } = await loadFixture(liquidate);
+            const lastLiquidationId = await liquidator.lastLiquidationId;
+            const lastLiquidation = await liquidator.liquidations(lastLiquidationId);
+            const loan = await bank.loans(loanId);
+            expect(lastLiquidation.loanId).to.equal(loanId);
+            expect(lastLiquidation.collateral).to.equal(loan.collateral);
+            expect(lastLiquidation.amount).to.equal(loan.amont);
+            expect(lastLiquidation.state).to.equal(LiquidationState.ACTIVE);
         });
     });
 });
