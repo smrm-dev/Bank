3b2f3467f88cb08a2f1f85aa775b93fb9253c103
smrm-dev <mrmousavi78@gmail.com>
2022-09-19 22:00:41 +0430

    Change harhdat config for testing on polygon

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,34 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-contract-sizer');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://polygon-rpc.com",
+        blockNumber: 33310114
+      },
+    },
+  },
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100000
+          }
+        },
+      }
+    ],
+  },
+
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
 };
